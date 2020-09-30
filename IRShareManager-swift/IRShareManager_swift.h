717f74c6f4212f033dd980712232a5e708f63633
Phil <phil@PhildeMac-mini.engenius.login>
2020-09-30 17:35:37 +0800

    First commit

---
+++ IRShareManager-swift/IRShareManager_swift.h
@@ -0,0 +1,19 @@
+//
+//  IRShareManager_swift.h
+//  IRShareManager-swift
+//
+//  Created by Phil on 2020/9/30.
+//  Copyright © 2020 Phil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IRShareManager_swift.
+FOUNDATION_EXPORT double IRShareManager_swiftVersionNumber;
+
+//! Project version string for IRShareManager_swift.
+FOUNDATION_EXPORT const unsigned char IRShareManager_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IRShareManager_swift/PublicHeader.h>
+
+
